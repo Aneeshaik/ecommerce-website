c55972e6c3ca09a229891dc68423841cb1a3d4cf
Aneeshaik <aneesnafeesimmu@gmail.com>
2026-01-03 08:12:05 +0530

    navbar-created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom'
+import blackLogo from '../assets/logo-black.png'
+import { MapPin, Search, ShoppingCart, CircleUserRound } from 'lucide-react'
+
+const Navbar = () => {
+    return (
+        <div className='flex items-center justify-between w-full'>
+            <img src={blackLogo} alt="Wearly Logo" className="w-32 mx-auto"/>
+            <div className='relative w-125'>
+                <input 
+                    type="text"
+                    placeholder="Search for products, brands and more" 
+                    className="w-full p-2 bg-[#F2F3F7] rounded-4xl mx-auto text-black text-sm focus:outline-none"
+                />
+                <div className='absolute right-1 bg-[#018FFF] rounded-full p-1 top-1/2 transform -translate-y-1/2'>
+                    <Search size={18}/>
+                </div>
+            </div>
+            <div className='flex items-center gap-4'>
+                <div className='flex items-center gap-1'>
+                    <MapPin color='#91939B' />
+                    <div className='flex flex-col items-start'>
+                        <p className='text-[#91939B] text-xs font-semibold'>Delivering to Bengaluru</p>
+                        <p className='text-[#535759] font-semibold text-xs'>Update Location</p>
+                    </div>
+                </div>
+                <Link
+                    to="/#"
+                    className="flex items-center gap-1"
+                >
+                    <ShoppingCart color='#91939B' size={20} />
+                    <span className="text-sm font-medium text-[#535759]">Cart</span>
+                </Link>
+                <Link
+                    to="/#"
+                    className="flex items-center gap-1"
+                >
+                    <CircleUserRound color='#91939B' size={20} />
+                    <span className="text-sm font-medium text-[#535759]">Sign In</span>
+                </Link>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar
