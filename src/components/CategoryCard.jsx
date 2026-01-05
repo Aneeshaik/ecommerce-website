78d700f77f15103bc45dca9c86e0f02ea6fb16bd
Aneeshaik <aneesnafeesimmu@gmail.com>
2026-01-05 12:29:50 +0530

    reduced-category-image-height

--- src/components/CategoryCard.jsx
+++ src/components/CategoryCard.jsx
@@ -4,9 +4,8 @@ const CategoryCard = ({ title, image }) => {
     return(
         <Link to="#">
             <div className="flex flex-col gap-1 items-center">
-                <div className="bg-[#e4e6ee] w-44 h-44 rounded-full">
+                <div className="bg-[#e4e6ee] w-34 h-34 rounded-full">
                     <img src={image} alt="Category" className="object-contain w-full h-full rounded-full"/>
-
                 </div>
                 <p className="text-black font-semibold text-[18px]">{title}</p>
             </div>
